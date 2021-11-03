5cf4504a218d7b8fb187ca32b020d1ef58bb0ae9
Glitch (glitch-hello-website) <none>
2021-11-03 02:34:30 +0000

    🎯🍲 Checkpoint ./script.js:26026384/148 ./style.css:26026384/24

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ Make the "Click me!" button move when the visitor clicks it:
 const btn = document.querySelector("button"); // Get the button from the page
 // Detect clicks on the button
 if (btn) {
-  btn.onclick = function("openlinks") {
+  btn.onclick = function(OPENLINKS) {
     // The JS works in conjunction with the 'dipped' code in style.css
     btn.classList.toggle("dipped");
   };
  } 
-{openlinks =("https://server-lan.glitch.me")}
+
+{OPENLINKS} ("https://server-lan.glitch.me")
 // This is a single line JS comment
 /*
 This is a comment that can span multiple lines 
