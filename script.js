cb2ee90b8ce9725c35cdc8a769a73c98d61795ca
Glitch (glitch-hello-website) <none>
2021-11-03 02:14:29 +0000

    🎌✋ Checkpoint ./script.js:26026384/22 ./style.css:26026384/48 ./index.html:26026384/199

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ console.log("Hello 🌎");
 Make the "Click me!" button move when the visitor clicks it:
 - First add the button to the page by following the "Next steps" in the README
 */
-const btn = document.querySelector("button 1"); // Get the button from the page
+const btn = document.querySelector("button"); // Get the button from the page
 // Detect clicks on the button
 if (btn) {
-  btn.onclick = function("teste") {
+  btn.onclick = function(openlinks) {
     // The JS works in conjunction with the 'dipped' code in style.css
     btn.classList.toggle("dipped");
   };
