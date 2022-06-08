3b138b652817277e1e575cc8c3cd8b5c472f8dd1
Glitch (server-lan) <none>
2022-06-08 22:03:12 +0000

    🗾⛰ Checkpoint ./script.js:26026384/102

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ console.log("Hello 🌎");
  
   function openForm() {
   document.getElementById("jschat").style.display = "block";
-    
+  console.log("chat aberto!");
 }
 
 function closeForm() {
   document.getElementById("jschat").style.display = "none";
+  console.log("chat fechado!");
 }
