334b0e56d69a373c1c21775964f0fb174ba0ce9c
Glitch (server-lan) <none>
2022-06-08 22:14:20 +0000

    🏪💂 Checkpoint ./index.html:26026384/1156 ./script.js:26026384/811 ./style.css:26026384/9

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 console.log("Hello 🌎");
  
-  function openForm() {
-  document.getElementById("jschat").style.display = "block";
+  function openChat(){
+  document.getElementById("jsadminchat").style.display = "block";
   console.log("chat aberto!");
 }
 
-function closeForm() {
-  document.getElementById("jschat").style.display = "none";
+function closeChat(){
+  document.getElementById("jsadminchat").style.display = "none";
   console.log("chat fechado!");
 }
+
+var batepapoDescer = document.querySelector("#jsDescerBatePapo");
+batepapoDescer.addEventListener("click", function() {
+    window.scrollTo(0, 2000);
+  console.log("Usuario redirecionado para bate-papo!");
+});
