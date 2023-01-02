73bc8922bbedc9dcd5dd6220337951ab47e51c83
Glitch (script-em-branco-08) <none>
2023-01-02 23:36:49 +0000

    🐡🍱 Checkpoint ./main.js:26026384/917

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 function gerarNitro() {
     let code = '';
     let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    for(var i = 0; i < 16; i++){
-        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
-    }
   
   for(var abrir = 0; abrir < 5; abrir++){
+    for(var i = 0; i < 16; i++){
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));}
     code += '\n'
     window.open("https://discord.gift/" + code);
+    code = ""
   }
+  
+  
 } //generates codes
