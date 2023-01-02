4b5daf0cee624d21aaa088e7d6ab4533349f834b
Glitch (script-em-branco-08) <none>
2023-01-02 22:37:57 +0000

    🦄🏆 Checkpoint ./main.js:26026384/313

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
 document.getElementById('generate').addEventListener("click", generate);
 document.getElementById('clear').addEventListener("click", stop);
 
-(function(){
-    var oldLog = console.log;
-    console.log = function (message) {
-        document.getElementById('a').value += message
-        oldLog.apply(arguments);
-    };
-})();
 
 function download(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
   element.setAttribute('download', filename);
-
   element.style.display = 'none';
   document.body.appendChild(element);
-
   element.click();
-
   document.body.removeChild(element);
 }
+
+
 var textarea = document.getElementById('a')
-if (navigator.userAgent.indexOf('Mobile') !== -1) { document.getElementById('save').style.display = "none"; textarea.style.height = "500px";
+if (navigator.userAgent.indexOf('Mobile') !== -1) { 
+  document.getElementById('save').style.display = "none"; textarea.style.height = "500px";
 }//resize for mobile
 
 
@@ -34,9 +27,8 @@ getGiftCode = function () {
     for(var i = 0; i < 16; i++){
         code = code + dict.charAt(Math.floor(Math.random() * dict.length));
     }
-    console.log('https://discord.gift/' + code + "\n");
     code += '\n'
-    document.getElementById('b').value += "https://discord.gift/" + code;
+    window.open("https://discord.gift/" + code);
 
 } //generates codes
 
